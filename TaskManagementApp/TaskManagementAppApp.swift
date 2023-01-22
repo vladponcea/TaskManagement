4a7d211b8b8a5b284baff4a580c110c24568c8e0
vladponcea <vladponcea@icloud.com>
2023-01-22 11:55:21 +0200

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by Vladut Mihai Poncea on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
